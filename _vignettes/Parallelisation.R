f861efc0ec07f3ec65d20768e5b2a0ef2dfc30d7
Forner, Karl <karl.forner@precisionformedicine.com>
2023-06-01 18:00:51 +0200

    removed docs and moved vignettes one dir up.

--- _vignettes/Parallelisation.R
+++
@@ -1,7 +0,0 @@
-## ---- include = FALSE, echo = FALSE, message =  FALSE, warning=FALSE----------
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>"
-)
-withr:::defer({Sys.unsetenv('EDP_PROFILE')}, parent.frame(n=2))
-
